db17f419d7841a60dc0e534a63695647248d3693
Mihail <7074033@gmail.com>
2020-07-11 09:43:36 +0300

    initial commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import "./styles/main.pcss";
+if (process.env.NODE_ENV === "development") {
+  require("file-loader!./index.pug");
+}
+import "bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
